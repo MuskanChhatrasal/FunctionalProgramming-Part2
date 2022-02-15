099df8c72e34461d483b5b610f3cabd8b55c298f
Muskan Chhatrasal <76219453+MuskanChhatrasal@users.noreply.github.com>
2022-02-15 06:18:07 +0000

    Done Question2

--- index.js
+++ index.js
@@ -6,3 +6,35 @@
   // console.log(arr.map(isEven));
 
 // ------------------------------------------------
+
+// Q2. Get the names in an array for only those who have a cycle.
+
+// const family = [
+//   {
+//     name    : 'Tanay',
+//     haveCycle : true
+//   },
+//   {
+//     name     : 'Akanksha',
+//     haveCycle : false
+//   },
+//   {
+//     name     : 'Tanvi',
+//     haveCycle : true
+//   },
+// 	{
+//     name     : 'Kanak',
+//     haveCycle : false
+//   }
+// ];
+
+// Method1
+// const isCycleTrue = obj => obj.haveCycle===true;
+// const haveCycleTrue = family.filter(isCycleTrue);
+// const haveCycleNames = obj => obj.name;
+// console.log(haveCycleTrue.map(haveCycleNames));
+
+// Method2
+// const currObj={};
+// const reducer = (ifCycle,currObj) => currObj.haveCycle===true?[...ifCycle,currObj.name]:[...ifCycle];
+// console.log(family.reduce(reducer,[]));
