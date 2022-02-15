cfb6fe9791479615c64482bbbcff6e6568761d31
Muskan Chhatrasal <76219453+MuskanChhatrasal@users.noreply.github.com>
2022-02-15 05:35:12 +0000

    Done Question1

---
+++ index.js
@@ -0,0 +1,8 @@
+// Q1. Given an array. Write a function to change all even numbers in an array to odd numbers by adding 1 to it.
+
+// Ans1: 
+  // const arr = [1, 2, 3, 58, 5, 6, 24, 8, 15, 4];
+  // const isEven = num => num%2===0? num+1:num;
+  // console.log(arr.map(isEven));
+
+// ------------------------------------------------
